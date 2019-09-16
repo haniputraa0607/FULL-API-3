75f1fc0a116e01bd5616348385d8721cbebc8489
Yogie Setya Nugraha <yogie@technopartner.id>
2019-09-16 10:24:50 +0700

    Add syncBrand & checkApi

--- Modules/Brand/Http/Controllers/ApiSyncBrandController.php
+++ Modules/Brand/Http/Controllers/ApiSyncBrandController.php
@@ -5,68 +5,89 @@ namespace Modules\Brand\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Routing\Controller;
+use DB;
+
+use Modules\Brand\Http\Requests\SyncBrand;
+use Modules\Brand\Entities\Brand;
+use App\Http\Models\Setting;
 
 class ApiSyncBrandController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     * @return Response
-     */
-    public function index()
+    function __construct()
     {
-        return view('brand::index');
+        date_default_timezone_set('Asia/Jakarta');
     }
 
-    /**
-     * Show the form for creating a new resource.
-     * @return Response
-     */
-    public function create()
+    public function syncBrand(SyncBrand $request)
     {
-        return view('brand::create');
+        $post = $request->json()->all();
+
+        $api = $this->checkApi($post['api_key'], $post['api_secret']);
+        if ($api['status'] != 'success') {
+            return response()->json($api);
+        }
+
+        DB::beginTransaction();
+
+        $countSave = 0;
+        $countUpdate = 0;
+        foreach ($post['brand'] as $key => $value) {
+            $data['name_brand'] = $value['name'];
+            $data['code_brand'] = strtoupper($value['code']);
+
+            $cekBrand = Brand::where('code_brand', strtoupper($value['code']))->first();
+
+            if ($cekBrand) {
+                $update = Brand::where('code_brand', strtoupper($value['code']))->update($data);
+                $countUpdate = $countUpdate + 1;
+                if (!$update) {
+                    DB::rollBack();
+                    return response()->json([
+                        'status'   => 'fail',
+                        'messages' => ['fail to sync']
+                    ]);
+                }
+            } else {
+                $save = Brand::create($data);
+                $countSave = $countSave + 1;
+                if (!$save) {
+                    DB::rollBack();
+                    return response()->json([
+                        'status'   => 'fail',
+                        'messages' => ['fail to sync']
+                    ]);
+                }
+            }
+        }
+
+        DB::commit();
+        return response()->json([
+            'status' => 'success', 'result' => ['inserted' => $countSave, 'updated' => $countUpdate]
+        ]);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     * @param  Request $request
-     * @return Response
-     */
-    public function store(Request $request)
+    function checkApi($key, $secret)
     {
-    }
+        $api_key = Setting::where('key', 'api_key')->first();
+        if (empty($api_key)) {
+            return ['status' => 'fail', 'messages' => ['api_key not found']];
+        }
 
-    /**
-     * Show the specified resource.
-     * @return Response
-     */
-    public function show()
-    {
-        return view('brand::show');
-    }
+        $api_key = $api_key['value'];
+        if ($api_key != $key) {
+            return ['status' => 'fail', 'messages' => ['api_key isn\’t match']];
+        }
 
-    /**
-     * Show the form for editing the specified resource.
-     * @return Response
-     */
-    public function edit()
-    {
-        return view('brand::edit');
-    }
+        $api_secret = Setting::where('key', 'api_secret')->first();
+        if (empty($api_secret)) {
+            return ['status' => 'fail', 'messages' => ['api_secret not found']];
+        }
 
-    /**
-     * Update the specified resource in storage.
-     * @param  Request $request
-     * @return Response
-     */
-    public function update(Request $request)
-    {
-    }
+        $api_secret = $api_secret['value'];
+        if ($api_secret != $secret) {
+            return ['status' => 'fail', 'messages' => ['api_secret isn\’t match']];
+        }
 
-    /**
-     * Remove the specified resource from storage.
-     * @return Response
-     */
-    public function destroy()
-    {
+        return ['status' => 'success'];
     }
 }
