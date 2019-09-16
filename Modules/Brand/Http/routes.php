0e1fabf479d4d00bfeff12c7280d0a03dd24bb30
Yogie Setya Nugraha <yogie@technopartner.id>
2019-09-16 15:24:09 +0700

    Add list brand

--- Modules/Brand/Http/routes.php
+++ Modules/Brand/Http/routes.php
@@ -3,5 +3,6 @@
 Route::group(['middleware' => ['auth:api'], 'prefix' => 'api/brand', 'namespace' => 'Modules\Brand\Http\Controllers'], function () {
     Route::get('/', 'ApiBrandController@index');
 
+    Route::get('list', 'ApiBrandController@listBrand');
     Route::post('sync', 'ApiSyncBrandController@syncBrand');
 });
