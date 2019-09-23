0451a7af11c454ad437206e4b80e2165b2bd84b7
Toto Prasetyo <toto@technopartner.id>
2019-09-23 08:55:18 +0700

    update job and celebrate list seeder

--- database/seeds/SettingJobsCelebrateSeeder.php
+++ database/seeds/SettingJobsCelebrateSeeder.php
@@ -15,14 +15,14 @@ class SettingJobsCelebrateSeeder extends Seeder
             0 => 
             array (
                 'key' => 'jobs_list',
-                'value_text' => '["Petani","Nelayan","Guru","Penjahit","Pengacara"]',
+                'value_text' => '["Karyawan Swasta", "Mengurus Rumah Tangga", "Pegawai Negeri Sipil", "Pelajar / Mahasiswa", "Wirausaha / Profesional", "Belum / Tidak Bekerja", "Lainnya"]',
                 'created_at' => date('Y-m-d H:i:s'),
                 'updated_at' => date('Y-m-d H:i:s'),
             ),
             1 => 
             array (
                 'key' => 'celebrate_list',
-                'value_text' => '["Idul Fitri","Idul Adha","Natal","Nyepi","Kliwonan","Weekend"]',
+                'value_text' => '["Idul Fitri", "Waisak", "Natal", "Imlek", "Galungan", "Lainnya"]',
                 'created_at' => date('Y-m-d H:i:s'),
                 'updated_at' => date('Y-m-d H:i:s'),
             )
