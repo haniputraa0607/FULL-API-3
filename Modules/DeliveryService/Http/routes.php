302948f67f7a68c1e1e07bf5b7c53eebfb5e128f
Yogie Setya Nugraha <yogie@technopartner.id>
2019-09-24 09:13:29 +0700

    Initial Commit Delivery Service

---
+++ Modules/DeliveryService/Http/routes.php
@@ -0,0 +1,5 @@
+<?php
+
+Route::group(['middleware' => 'web', 'prefix' => 'deliveryservice', 'namespace' => 'Modules\DeliveryService\Http\Controllers'], function () {
+    Route::get('/', 'ApiDeliveryServiceController@index');
+});
