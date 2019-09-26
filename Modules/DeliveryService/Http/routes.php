7bf859041b580fc5f6eabe5ec208ab7e0ac0c215
tpid-yogie <yogie@technopartner.id>
2019-09-26 14:24:09 +0700

    Update Route
    Add store route

--- Modules/DeliveryService/Http/routes.php
+++ Modules/DeliveryService/Http/routes.php
@@ -2,5 +2,6 @@
 
 Route::group(['middleware' => ['log_request', 'auth:api'], 'prefix' => 'api/delivery-service', 'namespace' => 'Modules\DeliveryService\Http\Controllers'], function () {
     Route::get('/', 'ApiDeliveryServiceController@index');
+    Route::post('store', 'ApiDeliveryServiceController@store');
     Route::get('webview', 'ApiDeliveryServiceController@detailWebview');
 });
