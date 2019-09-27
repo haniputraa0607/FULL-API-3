be7ab8e16d451df2fa2a0ae6f8074ef12e70e438
tpid-yogie <yogie@technopartner.id>
2019-09-27 14:14:15 +0700

    Create, Read, Update Route API Brand

--- Modules/Brand/Http/routes.php
+++ Modules/Brand/Http/routes.php
@@ -2,7 +2,10 @@
 
 Route::group(['middleware' => ['auth:api'], 'prefix' => 'api/brand', 'namespace' => 'Modules\Brand\Http\Controllers'], function () {
     Route::get('/', 'ApiBrandController@index');
-
     Route::get('list', 'ApiBrandController@listBrand');
+    Route::post('store', 'ApiBrandController@store');
+
+    Route::post('show', 'ApiBrandController@show');
+
     Route::post('sync', 'ApiSyncBrandController@syncBrand');
 });
