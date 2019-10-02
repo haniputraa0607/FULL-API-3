3e635c90a4c6807d964b1d0c3e54daf5a1bfb6a3
Yogie Setya Nugraha <yogie@technopartner.id>
2019-10-02 09:15:07 +0700

    Add new migration

---
+++ Modules/Enquiries/Database/Migrations/2019_10_02_090647_update_enquiries_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateEnquiriesTable extends Migration
+{
+    public function __construct()
+    {
+        DB::getDoctrineSchemaManager()->getDatabasePlatform()->registerDoctrineTypeMapping('enum', 'string');
+    }
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('enquiries', function (Blueprint $table) {
+            $table->unsignedInteger('id_outlet')->nullable()->change();
+            $table->timestamp('visiting_time')->nullable();
+            $table->string('position', 255)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('', function (Blueprint $table) {
+
+        });
+    }
+}
