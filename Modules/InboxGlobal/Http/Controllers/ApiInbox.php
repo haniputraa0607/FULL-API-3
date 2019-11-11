a4a61fedf4508fbfebea2298a6d33432f4769153
Toto Prasetyo <toto@technopartner.id>
2019-11-11 14:26:34 +0700

    remove fail on not update marked inbox

--- Modules/InboxGlobal/Http/Controllers/ApiInbox.php
+++ Modules/InboxGlobal/Http/Controllers/ApiInbox.php
@@ -218,18 +218,18 @@ class ApiInbox extends Controller
 			$userInbox = UserInbox::where('id_user_inboxes', $post['id_inbox'])->first();
 			if(!empty($userInbox)){
 				$update = UserInbox::where('id_user_inboxes', $post['id_inbox'])->update(['read' => '1']);
-				if(!$update){
-					$result = [
-						'status'  => 'fail',
-						'messages'  => ['Failed marked inbox']
-					];
-				}else{
+				// if(!$update){
+				// 	$result = [
+				// 		'status'  => 'fail',
+				// 		'messages'  => ['Failed marked inbox']
+				// 	];
+				// }else{
 					$countUnread = $this->listInboxUnread( $user['id']);
 					$result = [
 						'status'  => 'success',
 						'result'  => ['count_unread' => $countUnread]
 					];
-				}
+				// }
 			}else{
 				$result = [
 					'status'  => 'fail',
