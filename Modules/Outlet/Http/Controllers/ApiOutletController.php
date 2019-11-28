717685f8322b4b07c22cb6ca8747bfba166d5610
Dina <dina@technopartner.id>
2019-11-28 08:23:17 +0700

    update validation

--- Modules/Outlet/Http/Controllers/ApiOutletController.php
+++ Modules/Outlet/Http/Controllers/ApiOutletController.php
@@ -46,6 +46,7 @@ use Modules\Outlet\Http\Requests\outlet\DeletePhoto;
 use Modules\Outlet\Http\Requests\outlet\Nearme;
 use Modules\Outlet\Http\Requests\outlet\Filter;
 use Modules\Outlet\Http\Requests\outlet\OutletList;
+use Modules\Outlet\Http\Requests\outlet\OutletListOrderNow;
 
 use Modules\Outlet\Http\Requests\UserOutlet\Create as CreateUserOutlet;
 use Modules\Outlet\Http\Requests\UserOutlet\Update as UpdateUserOutlet;
@@ -1763,85 +1764,75 @@ class ApiOutletController extends Controller
         return MyHelper::checkGet($outlet);
     }
 
-    public function listOutletOrderNow(Request $request){
+    public function listOutletOrderNow(OutletListOrderNow $request){
         $post = $request->json()->all();
 
-        if(isset($post['phone']) && isset($post['latitude']) && isset($post['longitude'])){
-            try{
-                $outlet = Outlet::join('transactions','outlets.id_outlet', '=', 'transactions.id_outlet')
-                    ->join('users', 'users.id', '=', 'transactions.id_user')
-                    ->selectRaw('outlets.*, 
+        try{
+            $outlet = Outlet::join('transactions','outlets.id_outlet', '=', 'transactions.id_outlet')
+                ->join('users', 'users.id', '=', 'transactions.id_user')
+                ->selectRaw('outlets.*, 
+                        (111.111 * DEGREES(ACOS(LEAST(1.0, COS(RADIANS(outlet_latitude))
+                             * COS(RADIANS(-7.787683799999999))
+                             * COS(RADIANS(outlet_longitude - 110.66456829999993))
+                             + SIN(RADIANS(outlet_latitude))
+                             * SIN(RADIANS(-7.787683799999999)))))) AS distance_in_km' )
+                ->with(['user_outlets','city','today', 'outlet_schedules', 'brands'])
+                ->where('users.phone',$post['phone'])
+                ->where('outlets.outlet_status', 'Active')
+                ->whereHas('brands',function($query){
+                    $query->where('brand_active','1');
+                })
+                ->orderBy('distance_in_km')
+                ->get()->toArray();
+
+            if(empty($outlet)){
+                $outlet = Outlet::selectRaw('outlets.*, 
                         (111.111 * DEGREES(ACOS(LEAST(1.0, COS(RADIANS(outlet_latitude))
                              * COS(RADIANS(-7.787683799999999))
                              * COS(RADIANS(outlet_longitude - 110.66456829999993))
                              + SIN(RADIANS(outlet_latitude))
                              * SIN(RADIANS(-7.787683799999999)))))) AS distance_in_km' )
                     ->with(['user_outlets','city','today', 'outlet_schedules', 'brands'])
-                    ->where('users.phone',$post['phone'])
                     ->where('outlets.outlet_status', 'Active')
                     ->whereHas('brands',function($query){
                         $query->where('brand_active','1');
                     })
                     ->orderBy('distance_in_km')
+                    ->limit(5)
                     ->get()->toArray();
+            }
 
-                if(empty($outlet)){
-                    $outlet = Outlet::selectRaw('outlets.*, 
-                        (111.111 * DEGREES(ACOS(LEAST(1.0, COS(RADIANS(outlet_latitude))
-                             * COS(RADIANS(-7.787683799999999))
-                             * COS(RADIANS(outlet_longitude - 110.66456829999993))
-                             + SIN(RADIANS(outlet_latitude))
-                             * SIN(RADIANS(-7.787683799999999)))))) AS distance_in_km' )
-                        ->with(['user_outlets','city','today', 'outlet_schedules', 'brands'])
-                        ->where('outlets.outlet_status', 'Active')
-                        ->whereHas('brands',function($query){
-                            $query->where('brand_active','1');
-                        })
-                        ->orderBy('distance_in_km')
-                        ->limit(5)
-                        ->get()->toArray();
-                }
+            if(count($outlet) > 0){
+                $loopdata=&$outlet;
+                $loopdata = array_map(function($var) use ($post){
+                    $var['url']=env('API_URL').'api/outlet/webview/'.$var['id_outlet'];
+                    if(($post['latitude']??false)&&($post['longitude']??false)){
+                        $var['distance']=number_format((float)$this->distance($post['latitude'], $post['longitude'], $var['outlet_latitude'], $var['outlet_longitude'], "K"), 2, '.', '').' km';
+                    }
+                    return $var;
+                }, $loopdata);
 
-                if(count($outlet) > 0){
-                    $loopdata=&$outlet;
-                    $loopdata = array_map(function($var) use ($post){
-                        $var['url']=env('API_URL').'api/outlet/webview/'.$var['id_outlet'];
-                        if(($post['latitude']??false)&&($post['longitude']??false)){
-                            $var['distance']=number_format((float)$this->distance($post['latitude'], $post['longitude'], $var['outlet_latitude'], $var['outlet_longitude'], "K"), 2, '.', '').' km';
-                        }
-                        return $var;
-                    }, $loopdata);
-
-                    $result = [
-                        'status' => 'success',
-                        'messages' => [],
-                        'result' => [
-                            'data' => $outlet
-                        ]
-                    ];
-                }else{
-                    $result = [
-                        'status' => 'fail',
-                        'messages' => [],
-                        'result' => [
-                            'data' => null
-                        ]
-                    ];
-                }
-
-            }catch (Exception $e) {
+                $result = [
+                    'status' => 'success',
+                    'messages' => [],
+                    'result' => [
+                        'data' => $outlet
+                    ]
+                ];
+            }else{
                 $result = [
                     'status' => 'fail',
-                    'messages' => ['something went wrong'],
+                    'messages' => [],
                     'result' => [
                         'data' => null
                     ]
                 ];
             }
-        }else{
+
+        }catch (Exception $e) {
             $result = [
                 'status' => 'fail',
-                'messages' => ['incompleted parameter'],
+                'messages' => ['something went wrong'],
                 'result' => [
                     'data' => null
                 ]
