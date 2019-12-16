0d8217395b46a2db5adcc7c07c3bd0f2e61d4764
Toto Prasetyo <toto@technopartner.id>
2019-12-16 17:31:01 +0700

    features seeder

--- database/seeds/FeaturesTableSeeder.php
+++ database/seeds/FeaturesTableSeeder.php
@@ -1578,6 +1578,22 @@ class FeaturesTableSeeder extends Seeder
                     'feature_module' => 'List User Fraud',
                     'created_at' => date('Y-m-d H:00:00'),
                     'updated_at' => date('Y-m-d H:00:00'),
+                ),
+            196 =>
+                array(
+                    'id_feature' => 197,
+                    'feature_type' => 'List',
+                    'feature_module' => 'Maximum Order',
+                    'created_at' => date('Y-m-d H:00:00'),
+                    'updated_at' => date('Y-m-d H:00:00'),
+                ),
+            197 =>
+                array(
+                    'id_feature' => 198,
+                    'feature_type' => 'Update',
+                    'feature_module' => 'Maximum Order',
+                    'created_at' => date('Y-m-d H:00:00'),
+                    'updated_at' => date('Y-m-d H:00:00'),
                 )
         ));
     }
