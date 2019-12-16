37747c363615a7951d01c757d6d54a90a2078a0a
Toto Prasetyo <toto@technopartner.id>
2019-12-16 10:54:35 +0700

    add per product price total

--- Modules/Transaction/Http/Controllers/ApiOnlineTransaction.php
+++ Modules/Transaction/Http/Controllers/ApiOnlineTransaction.php
@@ -1534,8 +1534,9 @@ class ApiOnlineTransaction extends Controller
             if(!isset($tree[$product['id_brand']]['name_brand'])){
                 $tree[$product['id_brand']] = Brand::select('name_brand','id_brand')->find($product['id_brand'])->toArray();
             }
+            $product['product_price_total'] = $product['qty'] * ($product['product_price']+$mod_price);
             $tree[$product['id_brand']]['products'][]=$product;
-            $subtotal += $product['qty'] * ($product['product_price']+$mod_price);
+            $subtotal += $product['product_price_total'];
         }
         foreach ($grandTotal as $keyTotal => $valueTotal) {
             if ($valueTotal == 'subtotal') {
