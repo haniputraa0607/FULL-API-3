2e7f371b89ba481d1940a975cecc45e7091e256e
Toto Prasetyo <toto@technopartner.id>
2019-12-17 15:45:59 +0700

    fix undefined index note

--- Modules/Transaction/Http/Controllers/ApiOnlineTransaction.php
+++ Modules/Transaction/Http/Controllers/ApiOnlineTransaction.php
@@ -1502,7 +1502,7 @@ class ApiOnlineTransaction extends Controller
             unset($product['photos']);
             $product['id_custom'] = $item['id_custom']??null;
             $product['qty'] = $item['qty'];
-            $product['note'] = $item['note'];
+            $product['note'] = $item['note']??'';
             // get modifier
             $mod_price = 0;
             $product['modifiers']=[];
