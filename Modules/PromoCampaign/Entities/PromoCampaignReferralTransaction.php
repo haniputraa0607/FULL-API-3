ca9e7f33c509c5b5798109d7786a15918dc1440f
Toto Prasetyo <toto@technopartner.id>
2020-01-31 11:23:49 +0700

    migration

---
+++ Modules/PromoCampaign/Entities/PromoCampaignReferralTransaction.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Modules\PromoCampaign\Entities;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PromoCampaignReferralTransaction extends Model
+{
+    protected $fillable = [];
+}
