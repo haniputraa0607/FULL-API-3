40c31e0dc272bdfe909437dcee57dcbeff01dad5
Toto Prasetyo <toto@technopartner.id>
2020-02-07 13:54:30 +0700

    api referral BE

--- Modules/PromoCampaign/Entities/PromoCampaignReferral.php
+++ Modules/PromoCampaign/Entities/PromoCampaignReferral.php
@@ -7,4 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class PromoCampaignReferral extends Model
 {
     protected $fillable = [];
+    public function promo_campaign() {
+    	return $this->belongsTo(PromoCampaign::class,'id_promo_campaign','id_promo_campaign');
+    }
 }
