8d9f2cd35181d42c235b84b4259b9a91486e025b
Dina <dina@technopartner.id>
2020-02-12 08:43:32 +0700

    add check log save

--- app/Http/Middleware/LogActivitiesMiddleware.php
+++ app/Http/Middleware/LogActivitiesMiddleware.php
@@ -22,7 +22,7 @@ class LogActivitiesMiddleware
 	$response = $next($request);
 
         $arrReq = $request->except('_token');
-        if(!isset($arrReq['log_save'])){
+        if(!isset($arrReq['log_save']) || $arrReq['log_save'] || $arrReq['log_save'] == 1){
 
             if(!isset($arrReq['page']) || (int)$arrReq['page'] <= 1){
 
