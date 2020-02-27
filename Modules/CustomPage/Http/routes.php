8553bd6d1093035557a4426fc9c8e61aa14e0626
Dina <dina@technopartner.id>
2020-02-27 14:54:05 +0700

    update route list custom page

--- Modules/CustomPage/Http/routes.php
+++ Modules/CustomPage/Http/routes.php
@@ -6,11 +6,11 @@ Route::group(['middleware' => ['auth:api', 'log_activities', 'user_agent', 'scop
     Route::post('detail', ['middleware' => 'feature_control:153', 'uses' =>'ApiCustomPageController@show']);
     Route::post('update', ['middleware' => 'feature_control:151', 'uses' =>'ApiCustomPageController@store']);
     Route::post('delete', ['middleware' => 'feature_control:152', 'uses' =>'ApiCustomPageController@destroy']);
-    Route::get('list_custom_page', ['middleware' => 'feature_control:149', 'uses' =>'ApiCustomPageController@listCustomPage']);
 });
 
 Route::group(['middleware' => ['auth:api', 'log_activities', 'scopes:apps'], 'prefix' => 'api/custom-page', 'namespace' => 'Modules\CustomPage\Http\Controllers'], function () {
     Route::get('list', 'ApiCustomPageController@index');
+    Route::get('list_custom_page', 'ApiCustomPageController@listCustomPage');
     Route::get('webview/{id}', 'ApiCustomPageController@webviewCustomPage');
     Route::get('webview/{id}', ['uses' => 'ApiCustomPageController@webviewCustomPage']);
 });
