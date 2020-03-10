22a36e482ecc48543b5a24569f911119dd20474f
Yogie Setya Nugraha <yogiesetyanugraha@gmail.com>
2020-03-10 16:53:04 +0700

    Update Achievement
    - Category
    - Group

--- Modules/Achievement/Routes/api.php
+++ Modules/Achievement/Routes/api.php
@@ -13,6 +13,11 @@ use Illuminate\Http\Request;
 |
 */
 
+Route::group(['middleware' => ['auth:api', 'log_activities', 'user_agent', 'scopes:be'], 'prefix' => 'achievement'], function () {
+    Route::any('category', 'ApiAchievement@category');
+    Route::any('create', 'ApiAchievement@create');
+});
+
 Route::middleware('auth:api')->get('/achievement', function (Request $request) {
     return $request->user();
 });
