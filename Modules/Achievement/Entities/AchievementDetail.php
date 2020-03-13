bf216f78335fc223dce4aaa9934863db66e8e253
Yogie Setya Nugraha <yogiesetyanugraha@gmail.com>
2020-03-13 09:49:08 +0700

    Update Achievement

--- Modules/Achievement/Entities/AchievementDetail.php
+++ Modules/Achievement/Entities/AchievementDetail.php
@@ -2,6 +2,9 @@
 
 namespace Modules\Achievement\Entities;
 
+use App\Http\Models\Outlet;
+use App\Http\Models\Product;
+use App\Http\Models\Province;
 use Illuminate\Database\Eloquent\Model;
 
 class AchievementDetail extends Model
@@ -9,7 +12,7 @@ class AchievementDetail extends Model
     protected $table = 'achievement_details';
 
     protected $primaryKey = 'id_achievement_detail';
-    
+
     protected $fillable = [
         'id_achievement_group',
         'name',
@@ -23,4 +26,17 @@ class AchievementDetail extends Model
         'different_outlet',
         'different_province'
     ];
+
+    public function product()
+    {
+        return $this->belongsTo('App\Http\Models\Product', 'id_product');
+    }
+    public function outlet()
+    {
+        return $this->belongsTo('App\Http\Models\Outlet', 'id_outlet');
+    }
+    public function province()
+    {
+        return $this->belongsTo('App\Http\Models\Province', 'id_province');
+    }
 }
