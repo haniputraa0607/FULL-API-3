7c0d627097694e31be757a760419c3e6997c8ed7
damar <damar@technopartner.id>
2020-05-27 16:46:54 +0700

    add autocrm claim deals point

--- Modules/Deals/Http/Controllers/ApiDealsClaimPay.php
+++ Modules/Deals/Http/Controllers/ApiDealsClaimPay.php
@@ -353,7 +353,7 @@ class ApiDealsClaimPay extends Controller
 
                         if (($pay['voucher']['payment_method']??false) == 'Balance') 
                         {
-	                        $autocrm = app($this->autocrm)->SendAutoCRM('Claim Paid Deals Success', $phone,
+	                        $autocrm = app($this->autocrm)->SendAutoCRM('Claim Point Deals Success', $phone,
 	                            [
 	                                'claimed_at'                => $voucher->claimed_at, 
 	                                'deals_title'               => $voucher->dealVoucher->deals->deals_title,
