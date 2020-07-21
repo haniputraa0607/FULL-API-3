c638257253105b0b09b08d28a1447def4911a192
Toto Prasetyo <toto@technopartner.id>
2020-07-21 11:18:49 +0700

    cron auto reject

--- database/seeds/FeaturesTableSeeder.php
+++ database/seeds/FeaturesTableSeeder.php
@@ -2621,6 +2621,16 @@ class FeaturesTableSeeder extends Seeder
                     'created_at' => '2018-05-10 08:00:00',
                     'updated_at' => '2018-05-10 08:00:00',
                 ),
+            261 =>
+                array(
+                    'id_feature' => 262,
+                    'feature_type' => 'Update',
+                    'feature_module' => 'Order Auto Reject Time',
+                    'show_hide' => 1,
+                    'order' => 115,
+                    'created_at' => date('Y-m-d H:00:00'),
+                    'updated_at' => date('Y-m-d H:00:00'),
+                ),
         ));
     }
 }
