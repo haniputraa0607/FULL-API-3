b3e7ffeaeeb152fe4ec30472bff36ed5665301e0
Toto Prasetyo <toto@technopartner.id>
2020-08-11 10:51:42 +0700

    fix ipay double notif

--- Modules/IPay88/Lib/IPay88.php
+++ Modules/IPay88/Lib/IPay88.php
@@ -297,6 +297,9 @@ class IPay88
 				$detailTrx = TransactionPickup::where('id_transaction', $id_transaction)->first();
             	switch ($data['Status']) {
             		case '1':
+            			if ($trx->transaction_payment_status == 'Completed') {
+            				break;
+            			}
 	                    $update = $trx->update(['transaction_payment_status'=>'Completed','completed_at'=>date('Y-m-d H:i:s')]);
 	                    if(!$update){
 		                    DB::rollBack();
@@ -347,6 +350,9 @@ class IPay88
             			break;
 
             		case '0':
+            			if ($trx->transaction_payment_status == 'Cancelled') {
+            				break;
+            			}
 			            MyHelper::updateFlagTransactionOnline($trx, 'cancel', $trx->user);
 	                    $update = $trx->update(['transaction_payment_status'=>'Cancelled','void_date'=>date('Y-m-d H:i:s')]);
 		                $trx->load('outlet_name');
@@ -424,6 +430,9 @@ class IPay88
     			$deals = Deal::where('id_deals',$deals_user->id_deals)->first();
             	switch ($data['Status']) {
             		case '1':
+            			if ($deals_user->paid_status == 'Completed') {
+            				break;
+            			}
 	                    $update = $deals_user->update(['paid_status'=>'Completed']);
 	                    if(!$update){
 		                    DB::rollBack();
@@ -454,6 +463,9 @@ class IPay88
             			break;
 
             		case '0':
+            			if ($deals_user->paid_status == 'Cancelled') {
+            				break;
+            			}
 			            if($deals_user->balance_nominal){
 			                $insertDataLogCash = app("Modules\Balance\Http\Controllers\BalanceController")->addLogBalance($deals_user->id_user, $deals_user->balance_nominal, $deals_user->id_deals_user, 'Claim Deals Failed');
 			                if (!$insertDataLogCash) {
@@ -505,6 +517,9 @@ class IPay88
     			$subscription = Subscription::where('id_subscription',$model->id_subscription)->first();
             	switch ($data['Status']) {
             		case '1':
+            			if ($subscription_user->paid_status == 'Completed') {
+            				break;
+            			}
 	                    $update = $subscription_user->update(['paid_status'=>'Completed']);
 	                    if(!$update){
 		                    DB::rollBack();
@@ -535,6 +550,9 @@ class IPay88
             			break;
 
             		case '0':
+            			if ($subscription_user->paid_status == 'Cancelled') {
+            				break;
+            			}
 			            if($subscription_user->balance_nominal){
 			                $insertDataLogCash = app("Modules\Balance\Http\Controllers\BalanceController")->addLogBalance($subscription_user->id_user, $subscription_user->balance_nominal, $subscription_user->id_subscription_user, 'Claim Subscription Failed');
 			                if (!$insertDataLogCash) {
