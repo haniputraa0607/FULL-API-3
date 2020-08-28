56f5ab39c629be8591d1f6e0e8becad2c16cce5e
tpid-hani <haniyah.imtiyaz@technopartner.id>
2020-08-28 11:24:35 +0700

    update format receipt number
    JIWA+[4 digit random][10 digit timestamp][4 digit id outlet]

--- Modules/Transaction/Http/Controllers/ApiOnlineTransaction.php
+++ Modules/Transaction/Http/Controllers/ApiOnlineTransaction.php
@@ -867,7 +867,7 @@ class ApiOnlineTransaction extends Controller
         }
 
         //update receipt
-        $receipt = 'JIWA+'.MyHelper::createrandom(4,'Angka').time().MyHelper::createrandom(10,'Angka').$insertTransaction['id_outlet'];
+        $receipt = 'JIWA+'.MyHelper::createrandom(4,'Angka').time().$insertTransaction['id_outlet'];
         $updateReceiptNumber = Transaction::where('id_transaction', $insertTransaction['id_transaction'])->update([
             'transaction_receipt_number' => $receipt
         ]);
