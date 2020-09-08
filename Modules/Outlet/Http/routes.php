4bc6a307f1fd642a1e5ec9c875d56e947d01cb9a
Dina <dina@technopartner.id>
2020-09-08 17:15:24 +0700

    update: -update setting "email send to" to global -add pagination in update visibility product

--- Modules/Outlet/Http/routes.php
+++ Modules/Outlet/Http/routes.php
@@ -35,6 +35,8 @@ Route::group(['prefix' => 'api/outlet', 'middleware' => ['log_activities', 'auth
 Route::group(['prefix' => 'api/outlet', 'middleware' => ['log_activities', 'auth:api','user_agent', 'scopes:be'], 'namespace' => 'Modules\Outlet\Http\Controllers'], function()
 {
     Route::any('be/list', ['middleware' => 'feature_control:24', 'uses' =>'ApiOutletController@listOutlet'])->name('outlet_be');
+    Route::any('be/list/product-detail', ['middleware' => 'feature_control:24', 'uses' =>'ApiOutletController@listOutletProductDetail']);
+    Route::any('be/list/product-special-price', ['middleware' => 'feature_control:24', 'uses' =>'ApiOutletController@listOutletProductSpecialPrice']);
     Route::any('be/filter', ['middleware' => 'feature_control:24', 'uses' =>'ApiOutletController@filter']);
     Route::any('list/code', ['middleware' => 'feature_control:24', 'uses' =>'ApiOutletController@getAllCodeOutlet']);
     Route::any('ajax_handler','ApiOutletController@ajaxHandler');
