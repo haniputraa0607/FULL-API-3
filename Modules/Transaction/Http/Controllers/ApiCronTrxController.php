1f666a0924c9e5e53ca9739e385743ea75bfc3cc
Toto Prasetyo <toto@technopartner.id>
2020-09-09 17:42:23 +0700

    fix refund autoreject

--- Modules/Transaction/Http/Controllers/ApiCronTrxController.php
+++ Modules/Transaction/Http/Controllers/ApiCronTrxController.php
@@ -592,7 +592,7 @@ class ApiCronTrxController extends Controller
                             $payMidtrans = TransactionPaymentMidtran::find($pay['id_payment']);
                             if ($payMidtrans) {
                                 if(MyHelper::setting('refund_midtrans')){
-                                    $refund = Midtrans::refund($order['transaction_receipt_number'],['reason' => $post['reason']??'']);
+                                    $refund = Midtrans::refund($payMidtrans['vt_transaction_id'],['reason' => $post['reason']??'']);
                                     $reject_type = 'refund';
                                     if ($refund['status'] != 'success') {
                                         DB::rollback();
@@ -619,7 +619,7 @@ class ApiCronTrxController extends Controller
                     if ($payMidtrans) {
                         $point = 0;
                         if(MyHelper::setting('refund_midtrans')){
-                            $refund = Midtrans::refund($order['transaction_receipt_number'],['reason' => $post['reason']??'']);
+                            $refund = Midtrans::refund($payMidtrans['vt_transaction_id'],['reason' => $post['reason']??'']);
                             $reject_type = 'refund';
                             if ($refund['status'] != 'success') {
                                 DB::rollback();
