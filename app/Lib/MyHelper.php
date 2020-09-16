b824575d58045383cb7954164f7dd2d7557d5706
Dina <dina@technopartner.id>
2020-09-16 12:13:56 +0700

    change sepember to september

--- app/Lib/MyHelper.php
+++ app/Lib/MyHelper.php
@@ -2257,7 +2257,7 @@ class MyHelper{
 		$replace = array ( 'Sen','Sel','Rab','Kam','Jum','Sab','Min',
 			'Senin','Selasa','Rabu','Kamis','Jumat','Sabtu','Minggu',
 			'Jan','Feb','Mar','Apr','Mei','Jun','Jul','Ags','Sep','Okt','Nov','Des',
-			'Januari','Februari','Maret','April','Juni','Juli','Agustus','Sepember',
+			'Januari','Februari','Maret','April','Juni','Juli','Agustus','September',
 			'Oktober','November','Desember',
 		);
 		$date = date ($date_format, $timestamp);
