b7d809813ba9716c9d81cf65a291d2cd1d6e3cba
Toto Prasetyo <toto@technopartner.co.id>
2020-10-22 13:58:54 +0700

    fix migration

--- Modules/Product/Database/Migrations/2020_10_22_115753_create_product_modifier_group_pivots_table.php
+++ Modules/Product/Database/Migrations/2020_10_22_115753_create_product_modifier_group_pivots_table.php
@@ -14,10 +14,12 @@ class CreateProductModifierGroupPivotsTable extends Migration
     public function up()
     {
         Schema::create('product_modifier_group_pivots', function (Blueprint $table) {
-            $table->unsignedInteger('id_product_modifier');
+            $table->unsignedInteger('id_product')->nullable();
+            $table->unsignedBigInteger('id_product_variant')->nullable();
             $table->unsignedInteger('id_product_modifier_group');
             $table->foreign('id_product_modifier_group', 'fk_ipmg_pmgp_pmg')->on('product_modifier_groups')->references('id_product_modifier_group')->onDelete('cascade');
-            $table->foreign('id_product_modifier', 'fk_ipm_pmgp_pm')->references('id_product_modifier')->on('product_modifiers')->onDelete('cascade');
+            $table->foreign('id_product', 'fk_ip_pmgp_pm')->references('id_product')->on('products')->onDelete('cascade');
+            $table->foreign('id_product_variant', 'fk_ipv_pmgp_pm')->references('id_product_variant')->on('product_variants')->onDelete('cascade');
         });
     }
 
