90a7d7e21b8300cab72ded242d283cb7bf73db60
Toto Prasetyo <toto@technopartner.co.id>
2021-03-02 09:21:34 +0700

    fix autoreject, add delivery validation

--- Modules/OutletApp/Http/Controllers/ApiOutletApp.php
+++ Modules/OutletApp/Http/Controllers/ApiOutletApp.php
@@ -1685,6 +1685,18 @@ class ApiOutletApp extends Controller
             ]);
         }
 
+        if ($order->picked_by != 'Customer') {
+            $pickup_gosend = TransactionPickupGoSend::where('id_transaction_pickup', $order->id_transaction_pickup)->first();
+            if($pickup_gosend && !in_array($pickup_gosend['latest_status']??false, ['no_driver', 'rejected', 'cancelled'])) {
+                return response()->json([
+                    'status'   => 'fail',
+                    'messages' => ['Driver has been booked'],
+                ]);
+            } else {
+                goto reject;
+            }
+        }
+
         if ($order->ready_at) {
             if ($order->picked_by == 'Customer') {
                 return response()->json([
