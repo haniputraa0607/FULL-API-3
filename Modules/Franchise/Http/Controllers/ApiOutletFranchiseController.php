b84e1641643816fcec907b12b0644d4801810f29
tpid-damar <damar@technopartner.co.id>
2021-03-25 13:59:13 +0700

    add send outlet pin to user franchise

--- Modules/Franchise/Http/Controllers/ApiOutletFranchiseController.php
+++ Modules/Franchise/Http/Controllers/ApiOutletFranchiseController.php
@@ -73,6 +73,7 @@ class ApiOutletFranchiseController extends Controller
 	{
 		$post = $request->json()->all();
 		$outlet = Outlet::where('id_outlet', $request->id_outlet)->first();
+		$user_franchise = $request->user();
 
 		if (!$outlet) {
 			$result = [
@@ -133,6 +134,15 @@ class ApiOutletFranchiseController extends Controller
 
 			if (isset($queue_data)) {
 				SendOutletJob::dispatch($queue_data)->allOnConnection('outletqueue');
+				if (isset($user_franchise->email)) {
+
+		        	$autocrm = app($this->autocrm)->SendAutoCRM(
+                        'Outlet Pin Sent User Franchise',
+                        $user_franchise->email,
+                        $queue_data[0], 
+                        null, false, false, 'franchise', 1, false
+                    );
+		        }
 			}
 		}
 
