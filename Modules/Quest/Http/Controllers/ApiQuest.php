3ea33074a7c698351880960a8ad71c32d21735d7
Toto Prasetyo <toto@technopartner.co.id>
2021-04-22 09:10:05 +0700

    Revert "[ADDED] add benefit to api claim benefit"
    This reverts commit c98808cb2458b66ca0239578c55a27ad8eff2ada.

--- Modules/Quest/Http/Controllers/ApiQuest.php
+++ Modules/Quest/Http/Controllers/ApiQuest.php
@@ -830,7 +830,7 @@ class ApiQuest extends Controller
         return true;
     }
 
-    public function checkQuestCompleted($quest, $id_user, $auto = false, &$errors = [], &$benefit = null)
+    public function checkQuestCompleted($quest, $id_user, $auto = false, &$errors = [])
     {
         if (is_numeric($quest)) {
             $quest = Quest::where('id_quest', $quest)->first();
@@ -847,17 +847,16 @@ class ApiQuest extends Controller
             return false;
         }
 
-        $benefit =  QuestBenefit::with('deals')->where(['id_quest' => $quest->id_quest])->first();
-        if (!$benefit) {
-            goto flag;
-        }
-
         $redemption = QuestUserRedemption::where(['id_quest' => $quest->id_quest, 'id_user' => $id_user, 'redemption_status' => 1])->first();
         if ($redemption) {
             $errors[] = 'Hadiah sudah di klaim';
             return false;
         }
 
+        $benefit =  QuestBenefit::where(['id_quest' => $quest->id_quest])->first();
+        if (!$benefit) {
+            goto flag;
+        }
 
         if (!$benefit->autoclaim_benefit && $auto) {
             // not autoclaim
@@ -1113,25 +1112,16 @@ class ApiQuest extends Controller
 
     public function claimBenefit(Request $request)
     {
-        $claim = $this->checkQuestCompleted($request->id_quest, $request->user()->id, false, $errors, $quest_benefit);
+        $claim = $this->checkQuestCompleted($request->id_quest, $request->user()->id, false, $errors);
 
-        $benefit = [
-            'type' => $quest_benefit->benefit_type
-        ];
-        if ($quest_benefit->benefit_type == 'voucher') {
-            $benefit['text'] = $quest_benefit->deals->deals_title;
-        } else {
-            $benefit['text'] = MyHelper::requestNumber($quest_benefit->value, '_POINT').' Poin';
-        }
         if ($claim) {
-            return ['status' => 'success', 'result' => ['benefit' => $benefit]];
+            return ['status' => 'success'];
         }
         return [
             'status' => 'fail',
             'messages' => $errors ?? [
                 'Failed claim benefit'
             ],
-            'result' => ['benefit' => $benefit]
         ];
     }
 
