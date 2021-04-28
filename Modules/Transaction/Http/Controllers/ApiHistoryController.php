78a21d2e38972337ffb494635da13b9fd3367a3c
Toto Prasetyo <toto@technopartner.co.id>
2021-04-28 11:22:31 +0700

    [FIXED] history balance, quest benefit

--- Modules/Transaction/Http/Controllers/ApiHistoryController.php
+++ Modules/Transaction/Http/Controllers/ApiHistoryController.php
@@ -1156,7 +1156,7 @@ class ApiHistoryController extends Controller
                 $dataList['id']      = $value['id_log_balance'];
                 $dataList['date']    = date('d M Y H:i', strtotime($value['created_at']));
                 $dataList['outlet'] = 'Hadiah Misi';
-                $dataList['amount'] = MyHelper::requestNumber($value['balance'], '_POINT');
+                $dataList['amount'] = '+ ' . MyHelper::requestNumber($value['balance'], '_POINT');
             } else {
                 $dataList['type']   = 'profile';
                 $dataList['id']      = $value['id_log_balance'];
