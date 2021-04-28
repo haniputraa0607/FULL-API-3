47f27d269c63bafaeb384cd0f6000cd1cad2b18d
Toto Prasetyo <toto@technopartner.co.id>
2021-04-28 10:53:26 +0700

    [FIXED] fix not claimed detail quest

--- Modules/Quest/Http/Controllers/ApiQuest.php
+++ Modules/Quest/Http/Controllers/ApiQuest.php
@@ -1269,7 +1269,11 @@ class ApiQuest extends Controller
         $result['benefit']['id_reference'] = $result['user_redemption']['id_reference'] ?? null;
 
         $details = QuestDetail::select('name', 'short_description', 'is_done', 'quest_details.id_quest', 'quest_details.id_quest_detail', 'quest_rule', 'id_product_category', 'different_category_product', 'id_product', 'id_product_variant_group', 'product_total', 'trx_nominal', 'trx_total', 'id_outlet', 'id_province', 'different_outlet', 'different_province', 'id_quest_user_detail', 'id_quest_user', 'id_user')
-            ->where(['quest_details.id_quest' => $quest->id_quest, 'id_user' => $id_user])
+            ->where(['quest_details.id_quest' => $quest->id_quest])
+            ->where(function($query) use ($id_user) {
+                $query->where(['id_user' => $id_user])
+                    ->orWhereNull('quest_user_details.id_quest_user_detail');
+            })
             ->leftJoin('quest_user_details', 'quest_details.id_quest_detail', 'quest_user_details.id_quest_detail')
             ->get();
 
