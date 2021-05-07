b0c64c704e66f071048b9d975f29e2fc53293d72
Dina <dina@technopartner.co.id>
2021-05-07 16:23:17 +0700

    promo payment gateway

--- Modules/Disburse/Resources/views/detail_export.blade.php
+++ Modules/Disburse/Resources/views/detail_export.blade.php
@@ -9,11 +9,11 @@
     </tr>
     <tr>
         <td width="30"><b>Total Gross Sales</b></td>
-        <td>: {{(float)$summary_fee['total_sub_total']+abs((float)$summary_fee['total_discount_bundling'])}}</td>
+        <td>: {{number_format(((float)$summary_fee['total_sub_total']+abs((float)$summary_fee['total_discount_bundling'])),2,",","")}}</td>
     </tr>
     <tr>
         <td width="30"><b>Total Discount + Promo</b></td>
-        <td>: {{abs($summary_fee['total_discount'])+$summary_fee['total_subscription']+abs($summary_fee['total_discount_delivery'])+abs((float)$summary_fee['total_discount_bundling'])}}</td>
+        <td>: {{number_format((abs($summary_fee['total_discount'])+$summary_fee['total_subscription']+abs($summary_fee['total_discount_delivery'])+abs((float)$summary_fee['total_discount_bundling'])+$summary_fee['total_promo_payment_gateway']),2,",","")}}</td>
     </tr>
     <tr>
         <td width="30"><b>Total Delivery</b></td>
@@ -21,7 +21,7 @@
     </tr>
     <tr>
         <td width="30"><b>Sub Total (Gross Sales + delivery - discount - promo)</b></td>
-        <td>: {{(float)$summary_fee['total_gross_sales']-$summary_fee['total_subscription']}}</td>
+        <td>: {{number_format(((float)$summary_fee['total_gross_sales']-$summary_fee['total_subscription']-$summary_fee['total_income_promo_payment_gateway']),2,",","")}}</td>
     </tr>
     <tr>
         <td width="30"><b>Total Fee Item</b></td>
@@ -29,25 +29,29 @@
     </tr>
     <tr>
         <td width="30"><b>Total MDR PG</b></td>
-        <td>: {{(float)$summary_fee['total_fee_pg']}}</td>
+        <td>: {{number_format((float)$summary_fee['total_fee_pg'],2,",","")}}</td>
     </tr>
     @if(isset($show_another_income) && $show_another_income == 1)
     <tr>
         <td width="30"><b>Total Income Promo</b></td>
-        <td>: {{(float)$summary_fee['total_income_promo']}}</td>
+        <td>: {{number_format((float)$summary_fee['total_income_promo'],2,",","")}}</td>
     </tr>
     <tr>
         <td width="30"><b>Total Income Subscription</b></td>
-        <td>: {{(float)$summary_fee['total_income_subscription']}}</td>
+        <td>: {{number_format((float)$summary_fee['total_income_subscription'],2,",","")}}</td>
     </tr>
     <tr>
         <td width="30"><b>Total Income Bundling Product</b></td>
-        <td>: {{(float)$summary_fee['total_income_bundling_product']??0}}</td>
+        <td>: {{number_format((float)$summary_fee['total_income_bundling_product']??0,2,",","")}}</td>
+    </tr>
+    <tr>
+        <td width="30"><b>Total Income Promo Payment Gateway</b></td>
+        <td>: {{number_format((float)$summary_fee['total_income_promo_payment_gateway']??0,2,",","")}}</td>
     </tr>
     @endif
     <tr>
         <td width="30"><b>Total Income Outlet</b></td>
-        <td>: {{(float)$summary_fee['total_income_outlet']}}</td>
+        <td>: {{number_format((float)$summary_fee['total_income_outlet'],2,",","")}}</td>
     </tr>
 </table>
 <br>
