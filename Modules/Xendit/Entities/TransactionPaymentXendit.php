c95c85e23bf509633c72f3747211dd3fc459d3c9
tpid-dina <dina@technopartner.co.id>
2022-06-17 10:23:52 +0700

    [add] integration payment xendit

---
+++ Modules/Xendit/Entities/TransactionPaymentXendit.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Modules\Xendit\Entities;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Http\Models\Transaction;
+use Modules\Transaction\Entities\TransactionGroup;
+
+class TransactionPaymentXendit extends Model
+{
+    public $primaryKey  = 'id_transaction_payment_xendit';
+    protected $fillable = [
+        'id_transaction',
+        'id_transaction_group',
+        'xendit_id',
+        'payment_id',
+        'external_id',
+        'business_id',
+        'phone',
+        'type',
+        'amount',
+        'expiration_date',
+        'failure_code',
+        'status',
+        'checkout_url',
+    ];
+    public $items = null;
+
+    public static $prettyText = [
+        'DANA' => 'DANA',
+        'OVO' => 'OVO',
+        'LINKAJA' => 'LinkAja',
+    ];
+
+    public function transaction_group()
+    {
+        return $this->belongsTo(TransactionGroup::class, 'id_transaction_group');
+    }
+
+    public function transaction()
+    {
+        return $this->belongsTo(Transaction::class, 'id_transaction');
+    }
+
+    public function pay(&$errors = [])
+    {
+        if ($this->checkout_url || $this->business_id) {
+            return true;
+        }
+        $xenditController = app('Modules\Xendit\Http\Controllers\XenditController');
+        $create = $xenditController->create($this->type, $this->external_id, $this->amount, [
+            'phone' => $this->phone,
+            'items' => $this->items,
+        ], $errors);
+        if ($create) {
+            $this->xendit_id = $create['id'] ?? null;
+            $this->business_id = $create['business_id'] ?? null;
+            $this->checkout_url = $create['invoice_url'] ?? null;
+            $this->external_id = $create['external_id'] ?? $this->external_id;
+            $this->status = $create['status'] ?? null;
+            $result = true;
+        } else {
+            $result = false;
+        }
+        $save = $this->save();
+        return $result;
+    }
+}
---
+++ Modules/Xendit/Routes/api.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::group(['prefix'=>'xendit'],function(){
+	Route::post('notif', 'XenditController@notif')->name('notif_xendit');
+});
+
+Route::group(['prefix'=>'xendit-account', 'middleware' => ['auth:api', 'log_activities', 'user_agent', 'scopes:be']], function(){
+	Route::get('/', 'XenditAccountController@index');
+	Route::post('detail', 'XenditAccountController@show');
+	Route::post('update', 'XenditAccountController@update');
+});
