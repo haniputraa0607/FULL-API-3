61c86e2b4aeeb75992fde3055a295b222639a66d
tpid-dian <dian@technopartner.co.id>
2022-06-20 10:02:46 +0700

    bugfix migration

--- Modules/UserRating/Database/Migrations/2021_10_12_103005_add_id_transaction_consultation_to_user_ratings.php
+++ Modules/UserRating/Database/Migrations/2021_10_12_103005_add_id_transaction_consultation_to_user_ratings.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddIdTransactionProductServiceToUserRatings extends Migration
+class AddIdTransactionConsultationToUserRatings extends Migration
 {
     /**
      * Run the migrations.
