ff6c55142c0e3772d6288e420dc5ee4f016c6dfa
tpid-dian <dian@technopartner.co.id>
2022-07-04 14:46:23 +0700

    fix migrations

--- database/migrations/2022_06_24_144528_create_transaction_consultation_recomendations_table.php
+++ database/migrations/2022_06_24_144528_create_transaction_consultation_recomendations_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateTransactionConsultationRecomendationTable extends Migration
+class CreateTransactionConsultationRecomendationsTable extends Migration
 {
     /**
      * Run the migrations.
