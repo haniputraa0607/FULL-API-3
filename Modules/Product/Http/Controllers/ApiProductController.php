36a5bfe068b389c4fbaa0584b39d3e17edc80637
raka yulian <raka@technopartner.co.id>
2023-01-02 15:31:23 +0700

    Update ApiProductController.php

--- Modules/Product/Http/Controllers/ApiProductController.php
+++ Modules/Product/Http/Controllers/ApiProductController.php
@@ -2286,15 +2286,15 @@ class ApiProductController extends Controller
                 $list = $list->orderBy('products.created_at', 'desc');
             } elseif ($sorting == 'recommendation') {
                 $list = $list->orderBy('products.product_recommendation_status', 'desc')->orderBy('products.product_name', 'asc');
-            } elseif($sorting == 'Rating Tertinggi'){
+            } elseif ($sorting == 'Rating Tertinggi') {
                  $list = $list->orderby('products.total_rating', 'desc');
-            } elseif($sorting == 'Rating Terendah'){
+            } elseif ($sorting == 'Rating Terendah') {
                  $list = $list->orderby('products.total_rating', 'asc');
-            } elseif($sorting == 'Terlaris'){
+            } elseif ($sorting == 'Terlaris') {
                  $list = $list->orderby('product_count_transaction', 'desc');
-            } elseif($sorting == 'Terbaru'){
+            } elseif ($sorting == 'Terbaru') {
                 $list = $list->orderBy('products.created_at', 'desc');
-            } elseif($sorting == 'Harga Tertinggi'){
+            } elseif ($sorting == 'Harga Tertinggi') {
                  $list = $list->where(function ($q) use ($post) {
                         $q->where(function ($query1) use ($post) {
                             $query1->orderby('product_global_price', 'desc')
@@ -2303,8 +2303,8 @@ class ApiProductController extends Controller
                         $q->orWhereHas('base_price_variant', function ($query2) use ($post) {
                             $query2->orderby('product_variant_group_price', 'desc');
                         });
-                    });
-            } elseif($sorting == 'Harga Terendah'){
+                 });
+            } elseif ($sorting == 'Harga Terendah') {
                  $list = $list->where(function ($q) use ($post) {
                         $q->where(function ($query1) use ($post) {
                             $query1->orderby('product_global_price', 'asc')
@@ -2313,9 +2313,8 @@ class ApiProductController extends Controller
                         $q->orWhereHas('base_price_variant', function ($query2) use ($post) {
                             $query2->orderby('product_variant_group_price', 'asc');
                         });
-                    });
+                 });
             }
-        
         } else {
             $list = $list->orderBy('product_count_transaction', 'desc');
         }
@@ -2369,7 +2368,7 @@ class ApiProductController extends Controller
                 });
             });
         }
-        
+
         if (isset($post['range']) && $post['range'] != null) {
             $start = 0;
             foreach ($post['range'] as $v) {
