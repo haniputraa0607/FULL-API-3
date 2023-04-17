372ed415529a1d51bc10e95037551701b8cbd77f
rakayl <raka@technopartner.co.id>
2023-04-17 14:24:59 +0700

    dev

--- Modules/Transaction/Http/Controllers/ApiOnlineTransaction.php
+++ Modules/Transaction/Http/Controllers/ApiOnlineTransaction.php
@@ -1653,11 +1653,11 @@ class ApiOnlineTransaction extends Controller
                     'status' => 'fail',
                     'messages' => ['Transaksi tidak dapat dibatalkan karena proses pembayaran sedang berlangsung']
                 ];
-                case 'xendit va':
+            case 'xendit va':
                 $dtXendit = TransactionPaymentXendit::where('id_transaction_group', $trx['id_transaction_group'])->first();
                 $trx->triggerPaymentCancelled();
                 return ['status' => 'success', 'result' => ['message' => 'Pembayaran berhasil dibatalkan']];
-                
+
 
                 return [
                     'status' => 'fail',
